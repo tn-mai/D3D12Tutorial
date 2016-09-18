87885071d79998bb7cd14da65dbc63655ae4d87a
tn_mai <rabitfiver@gmail.com>
2016-09-18 14:22:42 +0900

    Direct3Dの初期化を実装.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -2,11 +2,35 @@
 * @file Main.cpp
 */
 #include "stdafx.h"
+#include <wrl/client.h>
+
+using Microsoft::WRL::ComPtr;
+
+struct Direct3DStuff {
+	static const int frameBufferCount = 3;
+	int witdh;
+	int height;
+	bool fullScreen;
+	HWND hwnd;
+
+	ComPtr<ID3D12Device> device;
+	ComPtr<IDXGISwapChain3> swapChain;
+	ComPtr<ID3D12CommandQueue> commandQueue;
+	ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap;
+	ComPtr<ID3D12Resource> renderTargetList[frameBufferCount];
+	ComPtr<ID3D12CommandAllocator> commandAllocator[frameBufferCount];
+	ComPtr<ID3D12GraphicsCommandList> commandList;
+	ComPtr<ID3D12Fence> fenceList[frameBufferCount];
+	HANDLE fenceEvent;
+	UINT64 fenceValue[frameBufferCount];
+	int frameIndex;
+	int rtvDescriptorSize;
+};
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-bool Initialize();
-bool Update();
-void Finalize();
+bool Initialize(Direct3DStuff&);
+bool Update(Direct3DStuff&);
+void Finalize(Direct3DStuff&);
 
 /**
 * アプリケーションのエントリポイント.
@@ -68,7 +92,12 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prevInstance, PWSTR cmdLine, i
 	ShowWindow(hwnd, cmdShow);
 	UpdateWindow(hwnd);
 
-	if (!Initialize()) {
+	Direct3DStuff d3dStuff;
+	d3dStuff.witdh = width;
+	d3dStuff.height = height;
+	d3dStuff.fullScreen = fullScreen;
+	d3dStuff.hwnd = hwnd;
+	if (!Initialize(d3dStuff)) {
 		MessageBox(nullptr, L"DirectXの初期化に失敗", L"エラー", MB_OK | MB_ICONERROR);
 		return 0;
 	}
@@ -85,10 +114,10 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prevInstance, PWSTR cmdLine, i
 		}
 
 		// TODO: ここにアプリケーションのコードを書く.
-		Update();
+		Update(d3dStuff);
 	}
 
-	Finalize();
+	Finalize(d3dStuff);
 
 	return 0;
 }
@@ -129,20 +158,154 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 }
 
 /**
+* Direct3Dの初期化.
+*
+* @retval true 初期化成功.
+* @retval false 初期化失敗.
+*/
+bool Init3D(Direct3DStuff& d3dStuff)
+{
+	HRESULT hr;
+
+	IDXGIFactory4* dxgiFactory;
+	hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	// 機能レベル11を満たすハードウェアアダプタを検索し、そのデバイスインターフェイスを取得する.
+	IDXGIAdapter1* dxgiAdapter;
+	int adapterIndex = 0;
+	bool adapterFound = false;
+	while (dxgiFactory->EnumAdapters1(adapterIndex, &dxgiAdapter) != DXGI_ERROR_NOT_FOUND) {
+		DXGI_ADAPTER_DESC1 desc;
+		dxgiAdapter->GetDesc1(&desc);
+		if (!(desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)) {
+			hr = D3D12CreateDevice(dxgiAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr);
+			if (SUCCEEDED(hr)) {
+				adapterFound = true;
+				break;
+			}
+		}
+		++adapterIndex;
+	}
+	if (!adapterFound) {
+		return false;
+	}
+	hr = D3D12CreateDevice(dxgiAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(d3dStuff.device.GetAddressOf()));
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	// コマンドキューを作成.
+	// 通常、コマンドキューはデバイスごとに1つだけ作成する.
+	D3D12_COMMAND_QUEUE_DESC cqDesc = {};
+	hr = d3dStuff.device->CreateCommandQueue(&cqDesc, IID_PPV_ARGS(d3dStuff.commandQueue.GetAddressOf()));
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	// スワップチェーンを作成.
+	DXGI_SWAP_CHAIN_DESC1 scDesc = {};
+	scDesc.Width = d3dStuff.witdh;
+	scDesc.Height = d3dStuff.height;
+	scDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scDesc.SampleDesc.Count = 1;
+	scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scDesc.BufferCount = d3dStuff.frameBufferCount;
+	scDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	ComPtr<IDXGISwapChain1> tmpSwapChain;
+	hr = dxgiFactory->CreateSwapChainForHwnd(d3dStuff.commandQueue.Get(), d3dStuff.hwnd, &scDesc, nullptr, nullptr, tmpSwapChain.GetAddressOf());
+	if (FAILED(hr)) {
+		return false;
+	}
+	tmpSwapChain.As(&d3dStuff.swapChain);
+	d3dStuff.frameIndex = d3dStuff.swapChain->GetCurrentBackBufferIndex();
+
+	// RTV用のデスクリプタヒープ及びデスクリプタを作成.
+	D3D12_DESCRIPTOR_HEAP_DESC rtvDesc = {};
+	rtvDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvDesc.NumDescriptors = d3dStuff.frameBufferCount;
+	rtvDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	hr = d3dStuff.device->CreateDescriptorHeap(&rtvDesc, IID_PPV_ARGS(d3dStuff.rtvDescriptorHeap.GetAddressOf()));
+	if (FAILED(hr)) {
+		return false;
+	}
+	d3dStuff.rtvDescriptorSize = d3dStuff.device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = d3dStuff.rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	for (int i = 0; i < d3dStuff.frameBufferCount; ++i) {
+		hr = d3dStuff.swapChain->GetBuffer(i, IID_PPV_ARGS(d3dStuff.renderTargetList[i].GetAddressOf()));
+		if (FAILED(hr)) {
+			return false;
+		}
+		d3dStuff.device->CreateRenderTargetView(d3dStuff.renderTargetList[i].Get(), nullptr, rtvHandle);
+		rtvHandle.ptr += d3dStuff.rtvDescriptorSize;
+	}
+
+	// コマンドアロケータを作成.
+	// コマンドアロケータは描画中にGPUが実行する各コマンドを保持する.
+	// GPUが描画中のコマンドを破棄した場合、GPUの動作は未定義になってしまう. そのため、描画中はコマンドを保持し続ける必要がある.
+	// ここでは、各バックバッファにひとつづつコマンドアロケータを持たせる.
+	// これによって、あるバックバッファが描画中でも、他のバックバッファのためのコマンドを生成・破棄できるようにする.
+	for (int i = 0; i < d3dStuff.frameBufferCount; ++i) {
+		hr = d3dStuff.device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(d3dStuff.commandAllocator[i].GetAddressOf()));
+		if (FAILED(hr)) {
+			return false;
+		}
+	}
+
+	// コマンドリストを作成.
+	// コマンドアロケータと異なり、コマンドリストはコマンドキューが実行されたあとならいつでもリセットできる.
+	// バックバッファ毎に持つ必要がないため1つだけ作ればよい.
+	// そのかわり、描画したいバックバッファが変わる毎に、対応するコマンドアロケータを設定し直す必要がある.
+	hr = d3dStuff.device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, d3dStuff.commandAllocator[0].Get(), nullptr, IID_PPV_ARGS(d3dStuff.commandList.GetAddressOf()));
+	if (FAILED(hr)) {
+		return false;
+	}
+	d3dStuff.commandList->Close();
+
+	// フェンスとフェンスイベントを作成.
+	// DirectX 12では、GPUの描画終了を検出するためにフェンスというものを使う.
+	// フェンスはOSのイベントに関連付けることができるように設計されている.
+	// そこで、OSのイベントを作成しておき、実際にフェンスを設定するときに関連付けることにする.
+	// フェンスはコマンドキューに設置するが、ひとつのフェンスは１か所にしか設置できない.
+	// GPUの描画中は設置済みのフェンスを再利用できないため、バックバッファ毎にひとつのフェンスを用意する.
+	// 全てのフェンスが同時にイベントを起こすことはないため、イベントは使いわますことができる.
+	// そこで、イベントはひとつだけでよい.
+	for (int i = 0; i < d3dStuff.frameBufferCount; ++i) {
+		d3dStuff.fenceValue[i] = 0;
+		hr = d3dStuff.device->CreateFence(d3dStuff.fenceValue[i], D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(d3dStuff.fenceList[i].GetAddressOf()));
+		if (FAILED(hr)) {
+			return false;
+		}
+	}
+	d3dStuff.fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	if (!d3dStuff.fenceEvent) {
+		return false;
+	}
+
+	return true;
+}
+
+/**
 * DirectXの初期化.
 *
 * @retval true 初期化成功.
 * @retval false 初期化失敗.
 */
-bool Initialize()
+bool Initialize(Direct3DStuff& d3dStuff)
 {
+	if (!Init3D(d3dStuff)) {
+		return false;
+	}
 	return true;
 }
 
 /**
 * DirectXの破棄.
 */
-void Finalize()
+void Finalize(Direct3DStuff&)
 {
 }
 
@@ -152,7 +315,7 @@ void Finalize()
 * @retval true ゲームを続ける.
 * @retval false ゲームを終了する.
 */
-bool Update()
+bool Update(Direct3DStuff&)
 {
 	return true;
 }
